83cd93d8a45aaf5aef6796cc2896cd8fd46b6f28
Yusuf Can Mahmutoğlu <canyusuf_95@outlook.com>
2022-07-10 15:57:10 +0300

    files changed

--- 5.hafta-js1-odevi/clock.js
+++ 5.hafta-js1-odevi/clock.js
@@ -1,11 +1,9 @@
 document.getElementById("myName").innerHTML = prompt("Adınızı giriniz.").trim();
 
 function showTime() {
-    let date = new Date();
-    let days = ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"];
-    let clock = document.getElementById('myClock');
-    clock.innerHTML = `${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}:${('0' + date.getSeconds()).slice(-2)} ${days[date.getUTCDay()]}`
-    setTimeout(showTime, 1); // setTimeOut fonksiyonu kullanırken iki parametre kullanılır.
+    let date = new Date().toLocaleString('tr-TR');
+    document.getElementById('myClock').innerHTML = date;
+    setTimeout(showTime, 1000); // setTimeOut fonksiyonu kullanırken iki parametre kullanılır.
     // Birinci parametre çalışması istenen fonksiyonu, ikinci parametre ise ne kadar süre
     // sonra çalışması istendiğini ifade eder.
 }
